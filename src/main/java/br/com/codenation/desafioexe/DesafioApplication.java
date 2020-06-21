aafb3e808dd5bfdb0ccd4f81d172058baddd217f
Open <open@Valspe_Note_04.opencorporate.com.br>
2020-06-21 14:30:32 -0300

    Desafio Fibonacci - Aceleradev Java

---
+++ src/main/java/br/com/codenation/desafioexe/DesafioApplication.java
@@ -0,0 +1,25 @@
+package br.com.codenation.desafioexe;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.stream.Stream;
+
+import static java.util.stream.Stream.*;
+
+public class DesafioApplication {
+
+	public static List<Integer> fibonacci() {
+		List<Integer> arrayFibonacci = new ArrayList<>();
+		iterate(new int[]{0, 1}, n -> new int[]{n[1], n[0] + n[1]})
+				.limit(15)
+				.map(n -> n[0])
+				.forEach(arrayFibonacci::add);
+
+		return arrayFibonacci;
+	}
+
+	public static Boolean isFibonacci(Integer a) {
+
+		return fibonacci().contains(a);
+	}
+}
